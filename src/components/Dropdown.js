561f05231d120dfbd1a1bad1ca7b7ab880ae90e9
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-07-24 14:26:28 +0900

    Panel component added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if(!divEl.current) {
+        return;
+      };
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      };
+    };
+
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler);
+
+    }
+  }, []);
 
   // open/close dropdown (toggles isOpen to the opposite of what it currently is)
   const handleClick = () => {
@@ -19,16 +39,26 @@ function Dropdown({ options, value, onChange }) {
   };
 
   const renderedOptions = options.map((option) => {
-    return <div className="hover:bg-sky-100 rounded cursor-pointer p-1"onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>;
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
   });
 
-
   return (
-    <div className="w-48 relative">
-      <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
-      {/* if value is null, display "Select...", otherwise display the label of the selected option */}
-      {value?.label || "Select..."}
-      <GoChevronDown className="text-lg"/>
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {/* if value is null, display "Select...", otherwise display the label of the selected option */}
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
       </Panel>
       {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
