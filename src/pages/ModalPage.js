049fe5b6b89367144daab7c0cad44efbf97eb811
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-07-25 19:15:04 +0900

    Modal finished

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -13,10 +13,64 @@ function ModalPage() {
     setShowModal(false);
   };
 
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>I accept</Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>
+        The modal component is a reusable component that can be used to display
+        any content in a modal.
+      </p>
+    </Modal>
+  );
+
   return (
     <div>
-    <Button onClick={handleClick} primary>Open Modal</Button>
-      {showModal && <Modal onClose={handleClose} />}
+
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+
+      {showModal && modal}
+      <br/>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, ipsum sit amet aliquet luctus, nisl enim aliquam diam, id
+        aliquam diam diam vitae lorem. Nulla facilisi. Nulla facilisi. Nulla
+        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+      </p>
     </div>
   );
 }
